a138d81cb1ccc7e46b7c2bb5c80bab569a4d9eab
Donggeun Kim <soonduck3729@gmail.com>
2024-03-06 17:37:59 +0900

    feat: 프로젝트 생성

---
+++ Springboot-blog/src/test/java/com/example/Springbootblog/SpringbootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
