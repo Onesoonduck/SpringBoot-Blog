a0ebae4c23f27c18d6adbf685c5ebf66ef29bdf2
soonduck <soonduck3729@gmail.com>
2024-07-19 15:16:44 +0900

    feat: view DTO 추가

--- Springboot-blog/src/test/java/com/example/Springbootblog/SpringbootBlogApplicationTests.java
+++ Springboot-blog/src/test/java/com/example/Springbootblog/SpringbootBlogApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.Springbootblog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class SpringbootBlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
